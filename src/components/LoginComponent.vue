4ff05808331f393e16cce6b0d1134fe9e415933f
Suman Bandopadhyay <sumanbando006@gmail.com>
2021-12-04 23:12:12 +0530

    login page created

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-6 q-pa-md justify-center">
+    <q-card bordered square>
+      <div class="q-pa-md self-center text-center">
+        <h2>Login</h2>
+        <q-form>
+          <q-input label="Email/Phone" />
+          <q-input label="Password" type="password" />
+          <div class="q-ma-lg center">
+            <q-btn label="Login" type="submit" color="primary" />
+          </div>
+        </q-form>
+      </div>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  ame: "LoginComponent",
+  setup() {
+    return {};
+  },
+};
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding class="window-height window-width row justify-center items-center">
+    <login-component/>
+  </q-page>
+</template>
+
+<script>
+import LoginComponent from 'src/components/LoginComponent.vue'
+export default {
+  components: { LoginComponent },
+  name: 'Login',
+}
+</script>
